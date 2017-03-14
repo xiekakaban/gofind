e0a18e8153c3fd6e112521e073ac92ca9b2220b9
阮天波 <ruantianbo@ruantianbodeMacBook-Pro.local>
2017-03-14 21:32:09 +0800

    update_sql

---
+++ document/sql/create_table.sql
@@ -0,0 +1,12 @@
+create table IF NOT EXISTS tb_user(
+     id varchar(50) primary key,
+     username varchar(100) not null unique,
+     password varchar(64) not null,
+     email varchar(150),
+     weichat varchar(100),
+     gender varchar(1),
+     location varchar(50),
+     create_time TIMESTAMP default CURRENT_TIMESTAMP,
+     last_modify_time TIMESTAMP default CURRENT_TIMESTAMP,
+     role_type varchar(20) not null
+ ) ENGINE=InnoDB;
